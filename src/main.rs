8772e72b73e588b943f262a68cb78074ed4e18d4
tttmmmyyyy <39012639+tttmmmyyyy@users.noreply.github.com>
2022-07-19 23:00:46 +0900

    inkwell hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+/*
+
 use once_cell::sync::Lazy;
 use std::sync::Arc;
 use std::vec::Vec;
@@ -149,4 +151,58 @@ mod tests {
 
 fn main() {
     println!("Hello, world!");
+}
+
+ */
+
+use inkwell::context::Context;
+use inkwell::OptimizationLevel;
+
+fn main() {
+    let context = Context::create();
+    // moduleを作成
+    let module = context.create_module("main");
+    // builderを作成
+    let builder = context.create_builder();
+
+    // 型関係の変数
+    let i32_type = context.i32_type();
+    let i8_type = context.i8_type();
+    let i8_ptr_type = i8_type.ptr_type(inkwell::AddressSpace::Generic);
+
+    // printf関数を宣言
+    let printf_fn_type = i32_type.fn_type(&[i8_ptr_type.into()], true);
+    let printf_function = module.add_function("printf", printf_fn_type, None);
+
+    // main関数を宣言
+    let main_fn_type = i32_type.fn_type(&[], false);
+    let main_function = module.add_function("main", main_fn_type, None);
+
+    // main関数にBasic Blockを追加
+    let entry_basic_block = context.append_basic_block(main_function, "entry");
+    // builderのpositionをentry Basic Blockに設定
+    builder.position_at_end(entry_basic_block);
+
+    // ここからmain関数に命令をビルドしていく
+    // globalに文字列を宣言
+    let hw_string_ptr = builder.build_global_string_ptr("Hello, world!", "hw");
+    // printfをcall
+    builder.build_call(
+        printf_function,
+        &[hw_string_ptr.as_pointer_value().into()],
+        "call",
+    );
+    // main関数は0を返す
+    builder.build_return(Some(&i32_type.const_int(0, false)));
+
+    // JIT実行エンジンを作成し、main関数を実行
+    let execution_engine = module
+        .create_jit_execution_engine(OptimizationLevel::Aggressive)
+        .unwrap();
+    unsafe {
+        execution_engine
+            .get_function::<unsafe extern "C" fn()>("main")
+            .unwrap()
+            .call();
+    }
 }
