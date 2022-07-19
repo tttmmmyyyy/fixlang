143fcb10951c6d84d0582d916183a7aafa42517f
tttmmmyyyy <39012639+tttmmmyyyy@users.noreply.github.com>
2022-07-19 09:32:02 +0900

    ast

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,152 @@
+use once_cell::sync::Lazy;
+use std::sync::Arc;
+use std::vec::Vec;
+
+// data Expr
+//   = Var Var
+//   | Lit Literal
+//   | App Expr Expr
+//   | Lam Var Expr -- Both term and type lambda
+//   | Let Bind Expr
+//   | Case Expr Var Type [(AltCon, [Var], Expr)]
+//   | Type Type -- Used for type application
+
+// data Var = Id Name Type -- Term variable
+//   | TyVar Name Kind -- Type variable
+
+// data Type = TyVarTy Var
+//   | LitTy TyLit
+//   | AppTy Type Type
+//   | TyConApp TyCon [Type]
+//   | FunTy Type Type
+//   | ForAllTy Var Type
+
+// TODO: まず総称型を削除する（テンプレートを展開する）フェーズが入りそう
+
+enum Expr {
+    Var(Arc<Var>),
+    Lit(Arc<Literal>),
+    App(Arc<Expr>, Arc<Expr>),
+    Lam(Arc<Var>, Arc<Expr>),
+    Let(Arc<Var>, Arc<Expr>, Arc<Expr>),
+    // Caseはあとで
+    If(Arc<Expr>, Arc<Expr>, Arc<Expr>),
+    Type(Arc<Type>),
+}
+
+struct Literal {
+    value: String,
+    ty: Arc<Type>,
+}
+
+enum Var {
+    TermVar { name: String, ty: Arc<Type> },
+    TyVar { name: String, kind: Arc<Kind> },
+}
+
+enum Kind {
+    Star,
+    Arrow(Arc<Kind>, Arc<Kind>),
+}
+
+struct TyLit {
+    value: String,
+    kind: Arc<Kind>,
+}
+
+enum Type {
+    TyVar(Arc<Var>),
+    LitTy(Arc<TyLit>),
+    AppTy(Arc<Type>, Arc<Type>),
+    TyConApp(Arc<TyCon>, Vec<Type>),
+    FunTy(Arc<Type>, Arc<Type>),
+    ForAllTy(Arc<Var>, Arc<Type>),
+}
+
+enum TyCon {
+    Pair,
+}
+
+fn mk_lit_expr(value: &str, ty: Arc<Type>) -> Arc<Expr> {
+    let value = String::from(value);
+    Arc::new(Expr::Lit(Arc::new(Literal { value, ty })))
+}
+
+fn mk_lit_type(value: &str) -> Arc<Type> {
+    let value = String::from(value);
+    Arc::new(Type::LitTy(Arc::new(TyLit {
+        value,
+        kind: Arc::new(Kind::Star),
+    })))
+}
+
+fn mk_arrow_type(src: Arc<Type>, dst: Arc<Type>) -> Arc<Type> {
+    Arc::new(Type::FunTy(src, dst))
+}
+
+static KIND_STAR: Lazy<Arc<Kind>> = Lazy::new(|| Arc::new(Kind::Star));
+
+fn mk_tyvar_var(var_name: &str) -> Arc<Var> {
+    Arc::new(Var::TyVar {
+        name: String::from(var_name),
+        kind: KIND_STAR.clone(),
+    })
+}
+
+fn mk_tyvar_type(var_name: &str) -> Arc<Type> {
+    Arc::new(Type::TyVar(mk_tyvar_var(var_name)))
+}
+
+fn mk_forall_type(var_name: &str, ty: Arc<Type>) -> Arc<Type> {
+    Arc::new(Type::ForAllTy(mk_tyvar_var("a"), ty))
+}
+
+static INT_TYPE: Lazy<Arc<Type>> = Lazy::new(|| mk_lit_type("Int"));
+
+static FIX_INT_INT: Lazy<Arc<Expr>> = Lazy::new(|| {
+    mk_lit_expr(
+        "fixIntInt",
+        mk_arrow_type(
+            mk_arrow_type(
+                mk_arrow_type(INT_TYPE.clone(), INT_TYPE.clone()),
+                mk_arrow_type(INT_TYPE.clone(), INT_TYPE.clone()),
+            ),
+            mk_arrow_type(INT_TYPE.clone(), INT_TYPE.clone()),
+        ),
+    )
+});
+
+static FIX_A_TO_B: Lazy<Arc<Expr>> = Lazy::new(|| {
+    mk_lit_expr(
+        "fix",
+        mk_forall_type(
+            "a",
+            mk_forall_type(
+                "b",
+                mk_arrow_type(
+                    mk_arrow_type(
+                        mk_arrow_type(mk_tyvar_type("a"), mk_tyvar_type("b")),
+                        mk_arrow_type(mk_tyvar_type("a"), mk_tyvar_type("b")),
+                    ),
+                    mk_arrow_type(mk_tyvar_type("a"), mk_tyvar_type("b")),
+                ),
+            ),
+        ),
+    )
+});
+
+// MEMO:
+// Lazy組み込み型を導入して、fix : (Lazy a -> a) -> Lazy aとするべきではないか。
+// fix : ((() -> a) -> (() -> a)) -> (() -> a) と等価で、FIX_A_TO_Bで表せるのでとりあえず良いけど。Lazyにはキャッシュ機能を付けたほうが良い。
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
