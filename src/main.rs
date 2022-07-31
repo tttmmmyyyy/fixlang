d93992a1e2ab9c544bd110b57e231da7aabad78e
tttmmmyyyy <39012639+tttmmmyyyy@users.noreply.github.com>
2022-07-31 11:20:59 +0900

    use of push_builder

--- src/main.rs
+++ src/main.rs
@@ -997,70 +997,80 @@ fn generate_func_retain_obj<'c, 'm, 'b>(gc: &GenerationContext<'c, 'm, 'b>) -> F
     // TODO: Raise error when trying to retain object of refcnt is zero (which implies use of deallocate memory).
 }
 
-fn generate_func_release_obj<'c, 'm, 'b>(gc: &GenerationContext<'c, 'm, 'b>) -> FunctionValue<'c> {
-    let context = gc.context;
-    let module = gc.module;
-    let system_functions = &gc.system_functions;
-    let void_type = context.void_type();
-    let ptr_to_refcnt_type = ptr_to_refcnt_type(context);
+fn generate_func_release_obj<'c, 'm, 'b>(
+    gc: &mut GenerationContext<'c, 'm, 'b>,
+) -> FunctionValue<'c> {
+    let void_type = gc.context.void_type();
+    let ptr_to_refcnt_type = ptr_to_refcnt_type(gc.context);
     let func_type = void_type.fn_type(&[ptr_to_refcnt_type.into()], false);
-    let release_func = module.add_function("release_obj", func_type, None);
-    let mut bb = context.append_basic_block(release_func, "entry");
+    let release_func = gc.module.add_function("release_obj", func_type, None);
+    let mut bb = gc.context.append_basic_block(release_func, "entry");
 
-    let builder = context.create_builder();
-    builder.position_at_end(bb);
-    let ptr_to_obj = release_func.get_first_param().unwrap().into_pointer_value();
-    let ptr_to_control_block = ptr_to_obj.const_cast(ptr_to_control_block_type(context));
-    let ptr_to_refcnt = builder
-        .build_struct_gep(ptr_to_control_block, 0, "ptr_to_refcnt")
-        .unwrap();
-    let refcnt = builder.build_load(ptr_to_refcnt, "refcnt").into_int_value();
+    let builder = gc.context.create_builder();
+    let (mut new_gc, pop_gc) = gc.push_builder(&builder);
+    {
+        let gc = &mut new_gc;
+        builder.position_at_end(bb);
+        let ptr_to_obj = release_func.get_first_param().unwrap().into_pointer_value();
+        let ptr_to_control_block = ptr_to_obj.const_cast(ptr_to_control_block_type(gc.context));
+        let ptr_to_refcnt = builder
+            .build_struct_gep(ptr_to_control_block, 0, "ptr_to_refcnt")
+            .unwrap();
+        let refcnt = builder.build_load(ptr_to_refcnt, "refcnt").into_int_value();
 
-    if DEBUG_MEMORY {
-        // check if refcnt is positive
-        let zero = context.i64_type().const_zero();
-        let is_positive = builder.build_int_compare(
-            inkwell::IntPredicate::ULE,
-            refcnt,
-            zero,
-            "is_refcnt_positive",
-        );
-        let then_bb = context.append_basic_block(release_func, "error_refcnt_already_leq_zero");
-        let cont_bb = context.append_basic_block(release_func, "refcnt_positive");
-        builder.build_conditional_branch(is_positive, then_bb, cont_bb);
+        if DEBUG_MEMORY {
+            // check if refcnt is positive
+            let zero = gc.context.i64_type().const_zero();
+            let is_positive = builder.build_int_compare(
+                inkwell::IntPredicate::ULE,
+                refcnt,
+                zero,
+                "is_refcnt_positive",
+            );
+            let then_bb = gc
+                .context
+                .append_basic_block(release_func, "error_refcnt_already_leq_zero");
+            let cont_bb = gc
+                .context
+                .append_basic_block(release_func, "refcnt_positive");
+            builder.build_conditional_branch(is_positive, then_bb, cont_bb);
+
+            builder.position_at_end(then_bb);
+            build_panic("Release object whose refcnt is already zero.", gc);
+            builder.build_unconditional_branch(cont_bb);
+
+            bb = cont_bb;
+            builder.position_at_end(bb);
+        }
+
+        let one = gc.context.i64_type().const_int(1, false);
+        let refcnt = builder.build_int_sub(refcnt, one, "refcnt");
+        let zero = gc.context.i64_type().const_zero();
+        let is_refcnt_zero =
+            builder.build_int_compare(inkwell::IntPredicate::EQ, refcnt, zero, "is_refcnt_zero");
+        let then_bb = gc
+            .context
+            .append_basic_block(release_func, "refcnt_zero_after_release");
+        let cont_bb = gc.context.append_basic_block(release_func, "end");
+        builder.build_conditional_branch(is_refcnt_zero, then_bb, cont_bb);
 
         builder.position_at_end(then_bb);
-        // build_panic("Release object whose refcnt is already zero.", gc);
+        let ptr_to_dtor_ptr = builder
+            .build_struct_gep(ptr_to_control_block, 1, "ptr_to_dtor_ptr")
+            .unwrap();
+        let ptr_to_dtor = builder
+            .build_load(ptr_to_dtor_ptr, "ptr_to_dtor")
+            .into_pointer_value();
+
+        let dtor_func = CallableValue::try_from(ptr_to_dtor).unwrap();
+        builder.build_call(dtor_func, &[ptr_to_obj.into()], "call_dtor");
+        builder.build_free(ptr_to_refcnt);
         builder.build_unconditional_branch(cont_bb);
 
-        bb = cont_bb;
-        builder.position_at_end(bb);
+        builder.position_at_end(cont_bb);
+        builder.build_return(None);
     }
-
-    let one = context.i64_type().const_int(1, false);
-    let refcnt = builder.build_int_sub(refcnt, one, "refcnt");
-    let zero = context.i64_type().const_zero();
-    let is_refcnt_zero =
-        builder.build_int_compare(inkwell::IntPredicate::EQ, refcnt, zero, "is_refcnt_zero");
-    let then_bb = context.append_basic_block(release_func, "refcnt_zero_after_release");
-    let cont_bb = context.append_basic_block(release_func, "end");
-    builder.build_conditional_branch(is_refcnt_zero, then_bb, cont_bb);
-
-    builder.position_at_end(then_bb);
-    let ptr_to_dtor_ptr = builder
-        .build_struct_gep(ptr_to_control_block, 1, "ptr_to_dtor_ptr")
-        .unwrap();
-    let ptr_to_dtor = builder
-        .build_load(ptr_to_dtor_ptr, "ptr_to_dtor")
-        .into_pointer_value();
-
-    let dtor_func = CallableValue::try_from(ptr_to_dtor).unwrap();
-    builder.build_call(dtor_func, &[ptr_to_obj.into()], "call_dtor");
-    builder.build_free(ptr_to_refcnt);
-    builder.build_unconditional_branch(cont_bb);
-
-    builder.position_at_end(cont_bb);
-    builder.build_return(None);
+    pop_gc(new_gc);
     release_func
     // TODO: Add fence instruction for incrementing refcnt
     // TODO: Add code for leak detector
@@ -1116,8 +1126,9 @@ fn generate_system_functions<'c, 'm, 'b>(gc: &mut GenerationContext<'c, 'm, 'b>)
     );
     gc.system_functions
         .insert(SystemFunctions::RetainObj, generate_func_retain_obj(gc));
+    let release_func = generate_func_release_obj(gc);
     gc.system_functions
-        .insert(SystemFunctions::ReleaseObj, generate_func_release_obj(gc));
+        .insert(SystemFunctions::ReleaseObj, release_func);
 }
 
 fn execute_main_module<'ctx>(module: &Module<'ctx>) -> i32 {
