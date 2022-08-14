39d75becc88eb3ec79350d1c27a979f80a578f34
tttmmmyyyy <39012639+tttmmmyyyy@users.noreply.github.com>
2022-08-14 22:24:02 +0900

    Remove generated files such as ll, bc. so.

--- sanitizer/build.sh
+++ sanitizer/build.sh
@@ -1,6 +1,6 @@
 # cargo build --release -- --emit=llvm-bc
 # cargo build --release -- --emit=llvm-ir
 # rm -r target
-cargo rustc --release -- --emit=llvm-bc && cp ./target/release/deps/fixsanitizer*.bc ./fixsanitizer.bc
-cargo rustc --release -- --emit=llvm-ir && cp ./target/release/deps/fixsanitizer*.ll ./fixsanitizer.ll
+# cargo rustc --release -- --emit=llvm-bc && cp ./target/release/deps/fixsanitizer*.bc ./fixsanitizer.bc
+# cargo rustc --release -- --emit=llvm-ir && cp ./target/release/deps/fixsanitizer*.ll ./fixsanitizer.ll
 cargo rustc --release -- --emit=llvm-ir && cp ./target/release/deps/libfixsanitizer*.so ./libfixsanitizer.so
