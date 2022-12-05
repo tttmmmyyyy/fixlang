925aaa7ac2ffd338f14c9727a5ca1a600fa892f5
tttmmmyyyy <39012639+tttmmmyyyy@users.noreply.github.com>
2022-12-05 23:16:22 +0900

    remove comment

--- src/runner.rs
+++ src/runner.rs
@@ -128,60 +128,6 @@ fn run_module(mut program: FixModule, opt_level: OptimizationLevel) -> i64 {
         gc.builder().build_return(Some(&ret));
     }
 
-    // for (name, defn) in &program.global_symbol {
-    //     let ptr_to_obj_ty = ptr_to_object_type(&gc.context);
-    //     let ptr_name = format!("PtrTo{}", name.to_string());
-    //     let acc_fn_name = format!("Get{}", name.to_string());
-
-    //     // Add global variable.
-    //     let global_var = gc.module.add_global(ptr_to_obj_ty, None, &ptr_name);
-    //     let null = ptr_to_obj_ty.const_null().as_basic_value_enum();
-    //     global_var.set_initializer(&null);
-    //     let global_var = global_var.as_basic_value_enum().into_pointer_value();
-
-    //     // Add accessor function.
-    //     let acc_fn_type = ptr_to_obj_ty.fn_type(&[], false);
-    //     let acc_fn = gc
-    //         .module
-    //         .add_function(&acc_fn_name, acc_fn_type, Some(Linkage::External));
-
-    //     // Implement Accessor function.
-    //     let entry_bb = gc.context.append_basic_block(acc_fn, "entry");
-    //     gc.builder().position_at_end(entry_bb);
-    //     let ptr_to_obj = gc
-    //         .builder()
-    //         .build_load(global_var, "load_global_var")
-    //         .into_pointer_value();
-    //     let is_null = gc.builder().build_is_null(ptr_to_obj, "PtrToObjIsNull");
-    //     let init_bb = gc.context.append_basic_block(acc_fn, "ptr_is_null");
-    //     let end_bb = gc.context.append_basic_block(acc_fn, "ptr_is_non_null");
-    //     gc.builder()
-    //         .build_conditional_branch(is_null, init_bb, end_bb);
-
-    //     // If ptr is null, then create object and initialize the pointer.
-    //     gc.builder().position_at_end(init_bb);
-    //     let obj = gc.eval_expr(defn.expr.clone());
-    //     gc.builder().build_store(global_var, obj);
-    //     gc.builder().position_at_end(init_bb);
-    //     if SANITIZE_MEMORY {
-    //         // Mark this object as global.
-    //         let obj_id = gc.get_obj_id(obj);
-    //         gc.call_runtime(RuntimeFunctions::MarkGlobal, &[obj_id.into()]);
-    //     }
-    //     gc.builder().build_unconditional_branch(end_bb);
-
-    //     // Return object.
-    //     gc.builder().position_at_end(end_bb);
-    //     let ret = gc
-    //         .builder()
-    //         .build_load(global_var, "PtrToObj")
-    //         .into_pointer_value();
-    //     gc.builder().build_return(Some(&ret));
-
-    //     // Register the accessor function to gc.
-    //     gc.add_global_object(name.clone(), acc_fn);
-    // }
-
     // Add main function.
     let main_fn_type = context.i64_type().fn_type(&[], false);
     let main_function = module.add_function("main", main_fn_type, None);
