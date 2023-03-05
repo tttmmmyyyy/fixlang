2106267cc3ef7efcaae5bef77268c4bac7c4c00f
tttmmmyyyy <39012639+tttmmmyyyy@users.noreply.github.com>
2023-03-05 18:48:51 +0900

    improve sanitizer build scriopt

--- sanitizer/build.sh
+++ sanitizer/build.sh
@@ -3,4 +3,9 @@
 # rm -r target
 # cargo rustc --release -- --emit=llvm-bc && cp ./target/release/deps/fixsanitizer*.bc ./fixsanitizer.bc
 # cargo rustc --release -- --emit=llvm-ir && cp ./target/release/deps/fixsanitizer*.ll ./fixsanitizer.ll
+
+# MacOS:
+# cargo rustc --release -- --emit=llvm-ir && cp ./target/release/deps/libfixsanitizer*.dylib ./libfixsanitizer.so
+
+# Windows:
 cargo rustc --release -- --emit=llvm-ir && cp ./target/release/deps/libfixsanitizer*.so ./libfixsanitizer.so
