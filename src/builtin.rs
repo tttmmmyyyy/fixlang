fdfbdd3e295c9cb54949a120cec85168e835b77a
tttmmmyyyy <39012639+tttmmmyyyy@users.noreply.github.com>
2023-06-02 22:41:08 +0900

    improve error message

--- src/builtin.rs
+++ src/builtin.rs
@@ -1927,7 +1927,7 @@ pub fn struct_mod(
     let field = definition.get_field_by_name(field_name);
     if field.is_none() {
         error_exit(&format!(
-            "error: no field `{}` found in the struct `{}`.",
+            "Error: no field `{}` found in the struct `{}`.",
             &field_name,
             struct_name.to_string(),
         ));
