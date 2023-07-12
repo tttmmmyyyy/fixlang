12eec54af6bfdb64e44f847ec8c8c1d3fc43ba11
tttmmmyyyy <39012639+tttmmmyyyy@users.noreply.github.com>
2023-07-12 22:59:01 +0900

    update benchmark script

--- benchmark/bench_prime_loop.sh
+++ benchmark/bench_prime_loop.sh
@@ -3,4 +3,4 @@ g++ -O2 ./benchmark/prime_loop.cpp && time ./a.out
 
 echo ""
 echo "Fix:"
-cargo run -- build ./examples/prime_loop.fix && time ./examples/prime_loop
+cargo run -- build -f ./examples/prime_loop.fix && time ./a.out
