76a6044ab17e78a9baa1a0371c2f1d631e771ec1
tttmmmyyyy <39012639+tttmmmyyyy@users.noreply.github.com>
2023-12-13 21:09:09 +0900

    duplicate llvm passes

--- src/llvm_passes.rs
+++ src/llvm_passes.rs
@@ -33,4 +33,35 @@ pub fn add_passes<T: PassManagerSubType>(passmgr: &PassManager<T>) {
     passmgr.add_cfg_simplification_pass();
     passmgr.add_bit_tracking_dce_pass();
     passmgr.add_constant_merge_pass();
+
+    // Duplicate passes because we have been running optimization passes twice upto 0d6a35a0781156de192159e1bc72b22571d9feb0.
+    passmgr.add_scalar_repl_aggregates_pass();
+    passmgr.add_function_inlining_pass();
+    passmgr.add_cfg_simplification_pass();
+    passmgr.add_early_cse_pass();
+    passmgr.add_loop_deletion_pass();
+    passmgr.add_scalar_repl_aggregates_pass_ssa();
+    passmgr.add_global_dce_pass();
+    passmgr.add_scoped_no_alias_aa_pass();
+    passmgr.add_merge_functions_pass();
+    passmgr.add_global_optimizer_pass();
+    passmgr.add_aggressive_dce_pass();
+    passmgr.add_merge_functions_pass();
+    passmgr.add_global_dce_pass();
+    passmgr.add_loop_deletion_pass();
+    passmgr.add_strip_dead_prototypes_pass();
+    passmgr.add_constant_merge_pass();
+    passmgr.add_dead_store_elimination_pass();
+    passmgr.add_sccp_pass();
+    passmgr.add_loop_unroll_and_jam_pass();
+    passmgr.add_jump_threading_pass();
+    passmgr.add_strip_symbol_pass();
+    passmgr.add_tail_call_elimination_pass();
+    passmgr.add_partially_inline_lib_calls_pass();
+    passmgr.add_ipsccp_pass();
+    passmgr.add_constant_merge_pass();
+    passmgr.add_dead_store_elimination_pass();
+    passmgr.add_cfg_simplification_pass();
+    passmgr.add_bit_tracking_dce_pass();
+    passmgr.add_constant_merge_pass();
 }
