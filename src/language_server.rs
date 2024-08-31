3da4ff5922eb37396c0126a29e7dfd6197efd1ff
tttmmmyyyy <39012639+tttmmmyyyy@users.noreply.github.com>
2024-08-31 21:33:47 +0900

    handle initialize request

--- src/language_server.rs
+++ src/language_server.rs
@@ -1,8 +1,44 @@
+use lsp_types::{InitializeParams, InitializeResult};
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use serde_json::Value;
+
 use crate::constants::LSP_LOG_FILE_PATH;
 use std::{
     fs::File,
     io::{Read, Write},
 };
+
+pub const WRITE_LOG: bool = true;
+
+#[derive(Deserialize, Serialize)]
+pub struct JSONRPCMessage {
+    pub jsonrpc: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub id: Option<u32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub method: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub params: Option<Value>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub result: Option<Value>,
+}
+
+impl JSONRPCMessage {
+    pub fn new(
+        id: Option<u32>,
+        method: Option<String>,
+        params: Option<Value>,
+        result: Option<Value>,
+    ) -> Self {
+        JSONRPCMessage {
+            jsonrpc: "2.0".to_string(),
+            id,
+            method,
+            params,
+            result,
+        }
+    }
+}
 
 // Launch the language server
 pub fn launch_language_server() {
@@ -45,10 +81,6 @@ pub fn launch_language_server() {
             continue;
         }
         let content_length = content_length.unwrap();
-        write_log(
-            &mut log_file,
-            &format!("Content-Length: {}\n", content_length),
-        );
 
         // Read stdin upto an empty line.
         loop {
@@ -56,7 +88,8 @@ pub fn launch_language_server() {
             let res = stdin.read_line(&mut line);
             if res.is_err() {
                 write_log(&mut log_file, "Failed to read a line: \n");
-                write_log(&mut log_file, &format!("{:?}\n", res.unwrap_err()));
+                let e = res.unwrap_err();
+                write_log(&mut log_file, &format!("{:?}\n", e));
                 continue;
             }
             if line.trim().is_empty() {
@@ -84,10 +117,71 @@ pub fn launch_language_server() {
         let message = message.unwrap();
 
         // Write the message to the log file.
-        write_log(&mut log_file, "Message: \n");
-        write_log(&mut log_file, &message);
-        write_log(&mut log_file, "\n");
+        // write_log(&mut log_file, "Message: \n");
+        // write_log(&mut log_file, &message);
+        // write_log(&mut log_file, "\n");
+
+        // Parse the message as JSONRPCMessage.
+        let message: Result<JSONRPCMessage, _> = serde_json::from_str(&message);
+        if message.is_err() {
+            write_log(
+                &mut log_file,
+                "Failed to parse the message as JSONRPCMessage: \n",
+            );
+            write_log(&mut log_file, &format!("{:?}\n", message.err().unwrap()));
+            continue;
+        }
+        let message = message.unwrap();
+
+        // Depending on the method, handle the message.
+        if message.method.as_ref().unwrap() == "initialize" {
+            let params: Option<InitializeParams> =
+                parase_params(message.params.unwrap(), &mut log_file);
+            if params.is_none() {
+                continue;
+            }
+            handle_initialize(message.id.unwrap(), &params.unwrap(), &mut log_file);
+        }
     }
+}
+
+fn parase_params<T: DeserializeOwned>(params: Value, log_file: &mut File) -> Option<T> {
+    let params: Result<T, _> = serde_json::from_value(params);
+    if params.is_err() {
+        write_log(log_file, "Failed to parse the params: \n");
+        write_log(log_file, &format!("{:?}\n", params.err().unwrap()));
+        return None;
+    }
+    params.ok()
+}
+
+fn send_response<T: Serialize>(id: u32, result: Option<&T>) {
+    let msg = JSONRPCMessage::new(
+        Some(id),
+        None,
+        None,
+        result.map(|result| serde_json::to_value(result).unwrap()),
+    );
+    send_message(&msg);
+}
+
+fn send_notification<T: Serialize>(method: String, params: Option<&T>) {
+    let msg = JSONRPCMessage::new(
+        None,
+        Some(method),
+        params.map(|params| serde_json::to_value(params).unwrap()),
+        None,
+    );
+    send_message(&msg);
+}
+
+fn send_message(msg: &JSONRPCMessage) {
+    let msg = serde_json::to_string(msg).unwrap();
+    let content_length = msg.len();
+    println!("Content-Length: {}\r\n\r\n{}", content_length, msg);
+    std::io::stdout()
+        .flush()
+        .expect("Failed to flush the stdout.");
 }
 
 fn open_log_file() -> File {
@@ -110,7 +204,19 @@ fn open_log_file() -> File {
 }
 
 fn write_log(file: &mut File, message: &str) {
-    file.write_all(message.as_bytes())
-        .expect("Failed to write a message to the log file.");
-    file.flush().expect("Failed to flush the log file.");
+    if WRITE_LOG {
+        file.write_all(message.as_bytes())
+            .expect("Failed to write a message to the log file.");
+        file.flush().expect("Failed to flush the log file.");
+    }
+}
+
+// Handle "initialize" method.
+fn handle_initialize(id: u32, _params: &InitializeParams, log_file: &mut File) {
+    // Return empty capabilities.
+    let result = InitializeResult {
+        capabilities: Default::default(),
+        server_info: None,
+    };
+    send_response(id, Some(&result))
 }
