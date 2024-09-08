b9586f96f512560d9c6026a64ce685b9481a3fb2
tttmmmyyyy <39012639+tttmmmyyyy@users.noreply.github.com>
2024-09-08 20:52:26 +0900

    impl lsp

--- src/error.rs
+++ src/error.rs
@@ -160,18 +160,18 @@ pub fn error_exit_with_src(msg: &str, src: &Option<Span>) -> ! {
     error_exit(&str)
 }
 
-pub fn error_exit_with_srcs(msg: &str, srcs: &[&Option<Span>]) -> ! {
-    let mut str = String::default();
-    str += msg;
-    str += "\n";
-    for src in srcs {
-        match src {
-            None => {}
-            Some(v) => {
-                str += "\n";
-                str += &v.to_string();
-            }
-        }
-    }
-    error_exit(&str)
-}
+// pub fn error_exit_with_srcs(msg: &str, srcs: &[&Option<Span>]) -> ! {
+//     let mut str = String::default();
+//     str += msg;
+//     str += "\n";
+//     for src in srcs {
+//         match src {
+//             None => {}
+//             Some(v) => {
+//                 str += "\n";
+//                 str += &v.to_string();
+//             }
+//         }
+//     }
+//     error_exit(&str)
+// }
