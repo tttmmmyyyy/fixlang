f67d7253e80c561e409e69dfa9df8a84354de1b7
tttmmmyyyy <39012639+tttmmmyyyy@users.noreply.github.com>
2024-11-20 21:39:03 +0900

    update speedtest

--- benchmark/speedtest/run.sh
+++ benchmark/speedtest/run.sh
@@ -1,5 +1,7 @@
 set -e
 
+LOG_FILE=speedtest.log
+
 SCRIPT_DIR=$(dirname "$0")
 pushd $SCRIPT_DIR
 trap 'popd' EXIT
@@ -15,6 +17,13 @@ ESTIMATE=$(echo "$OUTPUT" | tail -n 1)
 pushd ../../
 COMMIT_HASH=$(git rev-parse HEAD)
 popd
+
+# If the last line of the log file start with the commit hash, remove it.
+if [ -f speedtest.csv ]; then
+    if [ "$(tail -n 1 speedtest.csv | cut -d ',' -f 1)" == "$COMMIT_HASH" ]; then
+        sed -i '$ d' speedtest.csv
+    fi
+fi
 
 # Append the commit hash and estimate to a file "sppedtest.csv"
 echo "$COMMIT_HASH, $ESTIMATE" >> speedtest.csv
