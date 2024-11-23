bcb671e45af8148004f3c0292bd7bfb84b75edcb
tttmmmyyyy <39012639+tttmmmyyyy@users.noreply.github.com>
2024-11-23 20:58:11 +0900

    optimize passes

--- passes_optimizer.py
+++ passes_optimizer.py
@@ -104,52 +104,69 @@ ADDED_PASSES_NUM = 10
 
 # All passes
 # Exclude:
-#  add_scalar_repl_aggregates_pass_with_threshold (because requires parameter),
-#  add_internalize_pass (because requires parameter, and breaks program),
-#  add_gvn_pass (segfaults),
-#  add_new_gvn_pass (breaks program)
-#  add_licm_pass (breaks program)
-#  add_early_cse_mem_ssa_pass (breaks program (Random module))
-#  add_merge_functions_pass (breaks program)
+#  scalar_repl_aggregates_pass_with_threshold (because requires parameter),
+#  internalize_pass (because requires parameter, and breaks program),
+#  strip_symbol_pass (because added by default),
+#  verifier_pass (because added by default),
+#  gvn_pass (segfaults),
+#  new_gvn_pass (breaks program)
+#  licm_pass (breaks program)
+#  early_cse_mem_ssa_pass (breaks program (Random module))
+#  merge_functions_pass (breaks program)
 PASSES = '''
-function_inlining_pass
-early_cse_pass
-function_inlining_pass
-scalar_repl_aggregates_pass_ssa
-sccp_pass
-instruction_simplify_pass
-loop_reroll_pass
-ipsccp_pass
-function_inlining_pass
-loop_unroll_pass
-correlated_value_propagation_pass
-strip_dead_prototypes_pass
-loop_unroll_and_jam_pass
-scalar_repl_aggregates_pass
-ind_var_simplify_pass
+aggressive_dce_pass
+aggressive_inst_combiner_pass
+alignment_from_assumptions_pass
+always_inliner_pass
+argument_promotion_pass
+basic_alias_analysis_pass
+bit_tracking_dce_pass
 cfg_simplification_pass
-scalar_repl_aggregates_pass
-partially_inline_lib_calls_pass
-instruction_combining_pass
-reassociate_pass
-loop_reroll_pass
-partially_inline_lib_calls_pass
-aggressive_inst_combiner_pass
-loop_deletion_pass
-loop_unroll_pass
-aggressive_inst_combiner_pass
-instruction_combining_pass
-loop_rotate_pass
-merged_load_store_motion_pass
-loop_deletion_pass
-loop_unroll_pass
+constant_merge_pass
+coroutine_cleanup_pass
+coroutine_early_pass
+coroutine_elide_pass
+coroutine_split_pass
+correlated_value_propagation_pass
+dead_arg_elimination_pass
+dead_store_elimination_pass
+demote_memory_to_register_pass
+early_cse_pass
+function_attrs_pass
 function_inlining_pass
-loop_idiom_pass
-slp_vectorize_pass
-ipsccp_pass
-early_cse_pass
-early_cse_pass
+global_dce_pass
+global_optimizer_pass
 ind_var_simplify_pass
+instruction_combining_pass
+instruction_simplify_pass
+ipsccp_pass
+jump_threading_pass
+loop_deletion_pass
+loop_idiom_pass
+loop_reroll_pass
+loop_rotate_pass
+loop_unroll_and_jam_pass
+loop_unroll_pass
+loop_unswitch_pass
+loop_vectorize_pass
+lower_expect_intrinsic_pass
+lower_switch_pass
+memcpy_optimize_pass
+merged_load_store_motion_pass
+partially_inline_lib_calls_pass
+promote_memory_to_register_pass
+prune_eh_pass
+reassociate_pass
+scalar_repl_aggregates_pass
+scalar_repl_aggregates_pass_ssa
+scalarizer_pass
+sccp_pass
+scoped_no_alias_aa_pass
+simplify_lib_calls_pass
+slp_vectorize_pass
+strip_dead_prototypes_pass
+tail_call_elimination_pass
+type_based_alias_analysis_pass
 '''
 
 
@@ -168,9 +185,14 @@ def write_source_file(passes):
             f.write('\n')
 
 
+def install_fix():
+    subprocess.run(['cargo', 'install', '--locked',
+                   '--path', '.'], capture_output=True)
+
+
 def run_benchmark(timeout=10):
     work_dir = "./benchmark/speedtest"
-    cp = subprocess.run(['cargo', 'run', '--', 'build', '--llvm-passes-file', SOURCE_FILE],
+    cp = subprocess.run(['fix', 'build', '--llvm-passes-file', '../../' + SOURCE_FILE],
                         capture_output=True, text=True, cwd=work_dir)
     if cp.returncode != 0:
         print('build failed.')
@@ -208,6 +230,8 @@ def add_log(phase, time, passes):
 
 
 def optimize():
+    install_fix()
+
     all_passes = get_all_passes()
     optimum_passes = INITIAL_PASSES.copy()
 
