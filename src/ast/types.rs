0993b55f9940cf025961477cd079347ae6623212
tttmmmyyyy <39012639+tttmmmyyyy@users.noreply.github.com>
2025-04-26 21:38:03 +0900

    Remove workaround for #55

--- src/ast/types.rs
+++ src/ast/types.rs
@@ -1390,8 +1390,7 @@ impl TypeNode {
     // Get hash value.
     pub fn hash(self: &Arc<TypeNode>) -> String {
         let type_string = self.to_string_normalize();
-        // format!("{:x}", md5::compute(type_string))
-        format!("[{}]", type_string)
+        format!("{:x}", md5::compute(type_string))
     }
 
     // Returns the list of predicates for this type to be well-formed.
