a35d00b28632300e1299fa4eb3f326ee167896e8
tttmmmyyyy <39012639+tttmmmyyyy@users.noreply.github.com>
2025-11-25 22:23:06 +0900

    impl opt index syntax (wip)

--- src/optimization/opt_index_syntax.rs
+++ src/optimization/opt_index_syntax.rs
@@ -1,6 +1,36 @@
 /*
+# Index Syntax Optimization
 
-この最適化は、インデックス構文を利用して作成されたASTノードのパターンを検出し、より効率的な形に変換します。
+## Overview
+
+This optimization detects patterns of AST nodes created using index syntax and transforms them into more efficient forms.
+
+AST nodes created using index syntax have the following form:
+
+```
+use(|f| obj.act0(act1(...actN(f)...)))
+```
+
+- `use` represents one of `Std::Indexable::iget`, `Std::Indexable::iset(v)`, or `Std::Indexable::imod(v)`.
+- `act{I}` represents `Std::Array::act(i)` or a struct field accessor `act_{field}`.
+
+When this pattern is found, it is transformed into the following expression:
+
+When `use` is `Std::Indexable::iget`:
+```
+obj.get0.get1....getN
+```
+Here, `get{I}` represents `Std::Array::@(i)` or a struct field accessor `get_{field}`.
+
+When `use` is `Std::Indexable::iset(v)`:
+```
+obj.mod0(mod1(...modN-1(setN(v))...))
+```
+
+When `use` is `Std::Indexable::imod(v)`:
+```
+obj.mod0(mod1(...modN(v)...))
+```
 
 */
 
