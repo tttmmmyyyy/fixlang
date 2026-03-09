823b839fe74c17cb104e3bc5abee547dd54028f0
tttmmmyyyy <39012639+tttmmmyyyy@users.noreply.github.com>
2026-03-09 21:50:29 +0900

    now fix check automatically install dependencies

--- src/commands/check.rs
+++ src/commands/check.rs
@@ -1,5 +1,4 @@
 use crate::configuration::{BuildConfigType, Configuration, SubCommand};
-use crate::dependency::lockfile::LockFileType;
 use crate::elaboration::elaborate_via_config;
 use crate::error::Errors;
 use crate::metafiles::project_file::ProjectFile;
@@ -12,11 +11,9 @@ pub fn check(mut config: Configuration) -> Result<(), Errors> {
     let proj_file = ProjectFile::read_root_file()?;
     proj_file.set_config(&mut config, false)?;
 
-    // Set up the configuration by the lock file.
+    // Automatically generate/update lock file and install dependencies.
     // Use Test mode to include test dependencies.
-    proj_file
-        .open_lock_file(LockFileType::from_build_config_type(BuildConfigType::Test))?
-        .set_config(&mut config)?;
+    proj_file.install_dependencies(&mut config, BuildConfigType::Test)?;
 
     // Set all source files as diagnostics target files.
     match &mut config.subcommand {
