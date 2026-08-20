7d63a53e8fdc83ea3a5cd93747c9b45cd5cbf515
tttmmmyyyy <39012639+tttmmmyyyy@users.noreply.github.com>
2026-08-20 13:38:48 +0900

    Revert "Carry an inlining request through the uncurried symbol"
    This reverts commit 89610d316048dbd95e36fb03585b809bb98776b6.

--- src/optimization/uncurry.rs
+++ src/optimization/uncurry.rs
@@ -57,7 +57,7 @@ pub fn run(fix_mod: &mut Program) {
                     generic_name,
                     ty,
                     expr: Some(expr.clone()),
-                    inline_into_callers: sym.inline_into_callers,
+                    inline_into_callers: false,
                 },
             );
         }
